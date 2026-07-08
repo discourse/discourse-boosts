5c7b1602cd47a2c32d845351ea3ad5be8eb75a3b
David Taylor <david@taylorhq.com>
2026-07-08 15:57:56 +0100

    manual changes

--- spec/lib/discourse_boosts/flag_seed_spec.rb
+++ spec/lib/discourse_boosts/flag_seed_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-RSpec.describe "Flag seed for DiscourseBoosts::Boost" do
+RSpec.describe "Flag seed for DiscourseBoosts::Boost" do # rubocop:disable RSpec/DescribeClass
   def run_seed
-    load Rails.root.join("plugins", "discourse-boosts", "db", "fixtures", "004_flags.rb")
+    load Rails.root.join("plugins", "discourse-boosts", "db", "fixtures", "004_flags.rb") # rubocop:disable Discourse/Plugins/UseRequireRelative
   end
 
   def remove_boost_from_all_flags
